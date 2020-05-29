ae6996ef6a2a9e5b68626231b908c6d334fe9437
fuzun <fuzun54@outlook.com>
2020-05-29 03:20:15 +0300

    Fixed python client setup file.

--- client/python/setup.py
+++ client/python/setup.py
@@ -2,9 +2,13 @@ from setuptools import setup
 
 setup(
     name='iot-facerecognition-client',
-    version='1.0.0',
-    packages=[''],
-    install_requires=['opencv-python', 'websocket'],
+    version='1.1.1',
+    py_modules=['iot_facerecognition_client'],
+    install_requires=['opencv-python', 'websocket-client', 'screeninfo'],
+    entry_points={
+        'console_scripts': [
+            'iot-facerecognition-client = iot_facerecognition_client:main'
+        ]},
     url='https://github.com/fuzun/iot-facerecognition',
     license='GPLv3',
     author='fuzun',
